aad45f1f51ca42dc93392710a8f7964ef7ebaff8
Genesis Aquino <genesis.aquino@popcode.com.br>
2022-04-26 23:33:54 -0300

    feature: implement in coroutines and mvvm

--- app/src/main/java/com/genesis/simplemorty/MainActivity.kt
+++ app/src/main/java/com/genesis/simplemorty/MainActivity.kt
@@ -1,88 +1,57 @@
 package com.genesis.simplemorty
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.genesis.simplemorty.databinding.ActivityMainBinding
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.picasso.Picasso
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
+    private val viewModel: SharedViewModel by lazy {
+        ViewModelProvider(this)[SharedViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://rickandmortyapi.com/api/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
+        viewModel.refreshCharacter(300)
+        viewModel.characterByIdLiveData.observe(this) { response ->
+            if (response == null) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Unsuccessful network call!!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@observe
+            }
 
-        val rickAndMortyService: RickAndMortyService =
-            retrofit.create(RickAndMortyService::class.java)
+            setupUi(response)
 
-        rickAndMortyService.getCharacterById(765)
-            .enqueue(object : Callback<GetCharacterByIdResponse> {
-                override fun onResponse(
-                    call: Call<GetCharacterByIdResponse>,
-                    response: Response<GetCharacterByIdResponse>
-                ) {
-                    Log.i("MainActivity", response.toString())
-                    when (response.isSuccessful) {
-                        true -> {
-                            Toast.makeText(
-                                this@MainActivity,
-                                "Unsuccessful network call!!",
-                                Toast.LENGTH_SHORT
-                            ).show()
+        }
+    }
 
-                            val body = response.body()!!
+    private fun setupUi(response: GetCharacterByIdResponse) {
+        binding?.run {
+            nameTextView.text = response.name
+            aliveTextView.text = response.status
+            speciesTextView.text = response.species
+            originTextView.text = response.origin.name
 
-                            binding?.run {
-                                nameTextView.text = body.name
-                                aliveTextView.text = body.status
-                                speciesTextView.text = body.species
-                                originTextView.text = body.origin.name
-
-                                when (body.gender.equals("male", true)) {
-                                    true -> {
-                                        genderImageView.setImageResource(R.drawable.ic_male_24)
-                                    }
-                                    false -> {
-                                        genderImageView.setImageResource(R.drawable.ic_famale_24)
-                                    }
-                                }
-
-                                Picasso.get().load(body.image).into(headerImageView)
-                            }
-                        }
-                        false -> {
-                            Toast.makeText(
-                                this@MainActivity,
-                                "Unsuccessful network call!!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            return
-                        }
-                    }
-
+            when (response.gender.equals("male", true)) {
+                true -> {
+                    genderImageView.setImageResource(R.drawable.ic_male_24)
                 }
-
-                override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
-                    Log.i("MainActivity", t.message ?: "Null message")
+                false -> {
+                    genderImageView.setImageResource(R.drawable.ic_famale_24)
                 }
-            })
+            }
+
+            Picasso.get().load(response.image).into(headerImageView)
+        }
     }
 }
